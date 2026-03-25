251fec67e589db5b754970ccb16126e462290aa9
Silvarious9862 <vbobrik0@gmail.com>
2026-03-25 17:40:24 +0300

    LAB 1 FULL

--- sem1/lab1/src/signals.py
+++ sem1/lab1/src/signals.py
@@ -1,13 +1,40 @@
 import numpy as np
-from typing import Tuple
+from scipy.io import wavfile
+from typing import Callable
 
 
-def generate_signals(fs: int = 44100, duration: float = 2.0) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
-    """Генерация двух тестовых периодических сигналов и временной оси.
-
-    Возвращает t, x, y.
+def generate_signal(name: str,
+                    func: Callable[[np.ndarray], np.ndarray],
+                    fs: int = 44100,
+                    duration: float = 3.0):
     """
-    t = np.arange(0, duration, 1.0 / fs)
-    x = 2 * np.sin(3 * t) + np.cos(2 * t + 1)
-    y = np.sin(5 * t) + 0.5 * np.cos(7 * t)
-    return t, x.astype(np.float64), y.astype(np.float64)
+    name  – имя WAV-файла
+    func  – функция сигнала: принимает массив t, возвращает массив s(t)
+    fs    – частота дискретизации
+    duration – длительность в секундах
+    пример вызова:
+    generate_signal('sig1.wav', lambda t: 2*np.sin(3*t) + np.cos(2*t + 1))
+    """
+    t = np.linspace(0, duration, int(fs * duration), endpoint=False)
+
+    s = func(t)                      
+
+    s_norm = s / np.max(np.abs(s))   # нормализация
+    s_int16 = (s_norm * 32767).astype(np.int16)
+
+    wavfile.write(name, fs, s_int16)
+
+
+def read_signal(name: str):
+    """
+    пример вызова:
+    fs1, x_list = read_signal('sig1.wav')
+    """
+    fs, data = wavfile.read(name)
+
+    if data.ndim > 1:
+        data = data[:, 0]
+
+    x = data.astype(np.float32) / 32767.0
+    
+    return fs, x
